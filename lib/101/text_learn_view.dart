c6a63d0d5b691523bf3f9ab803aa9c284034aec7
huseyin6 <huseyinkkarakaya@gmail.com>
2024-07-15 16:43:52 +0300

    text widget implemented

--- lib/101/text_learn_view.dart
+++ lib/101/text_learn_view.dart
@@ -1,13 +1,73 @@
 import "package:flutter/material.dart";
 
 class TextLearnView extends StatelessWidget {
-  const TextLearnView({super.key});
+  TextLearnView({super.key, this.username});
+  final String name = "Huseyin";
+  final String surname = "Karakaya";
+  final String? username;
+  final TextViewKeys keys = TextViewKeys();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
-      child: Text("abc"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Welcome, $name ${name.length}",
+            maxLines: 2,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+                wordSpacing: 2,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                decoration: TextDecoration.underline,
+                fontSize: 20,
+                letterSpacing: 2,
+                color: Colors.black),
+          ),
+          Text(
+            surname,
+            maxLines: 2,
+            textAlign: TextAlign.left,
+            style: TextViewStyles.welcomeStyle,
+          ),
+          Text(
+            "How are you?",
+            maxLines: 2,
+            textAlign: TextAlign.left,
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium
+                ?.copyWith(color: TextViewColors.welcomeColor),
+          ),
+          Text(username ?? ""),
+          Text(
+            keys.welcomeText,
+            style: TextViewStyles.welcomeStyle,
+          ),
+        ],
+      ),
     ));
   }
+}
+
+class TextViewStyles {
+  static TextStyle welcomeStyle = const TextStyle(
+      wordSpacing: 2,
+      fontStyle: FontStyle.italic,
+      fontWeight: FontWeight.w600,
+      decoration: TextDecoration.underline,
+      fontSize: 20,
+      letterSpacing: 2,
+      color: Colors.black);
+}
+
+class TextViewColors {
+  static Color get welcomeColor => Colors.indigo;
+}
+
+class TextViewKeys {
+  final String welcomeText = "Hello!";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light(),
-      home: const TextLearnView(),
+      home: TextLearnView(),
     );
   }
 }
