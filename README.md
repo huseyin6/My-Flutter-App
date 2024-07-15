a56baf8da8088e98c5f2c20c9d03fc15aedd4a46
huseyin6 <huseyinkkarakaya@gmail.com>
2024-07-15 13:43:06 +0300

    project started

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_application_1-101
+
+A new Flutter Mobile Application project that contents basics of Flutter.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ lib/101/text_learn_view.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class TextLearnView extends StatelessWidget {
+  const TextLearnView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text("abc"),
+    ));
+  }
+}
